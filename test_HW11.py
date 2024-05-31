2cff462e98872ca18eca7a34f025f682e43e1778
Annamaria <fricska12@gmail.com>
2024-05-31 17:25:33 +0200

    final version

--- test_HW11.py
+++ test_HW11.py
@@ -1,23 +1,41 @@
 #HW Part1
 
+def evaluate(board):
+    if "xxx" in board:
+        game_state = "x"  # X has won
+    elif "ooo" in board:
+        game_state = "o"  # o has won
+    elif "-" not in board:
+        game_state = "!"  # nobody won, draw
+    else:
+        game_state = "-"  # game is not finished
 
+    return game_state
 
+def test_evaluate1():
+    assert evaluate("--------xxx---------") == "x"
 
+def test_evaluate2():
+    assert evaluate("-oo-----------------") == "-"
 
-
+def test_evaluate3():
+    assert evaluate("--------------------") == "-"
 
 #HW Part2
 
+def move(board, mark, position):
 
+    return board[:position-1] + mark + board[position:]
 
+def test_move1():
+    assert move("--------xx----------", "o", 3) == "--o-----xx----------"
 
-
-
-
-
+def test_move2():
+    assert move("--------xx----------", "x", 4) == "---x----xx----------"
 
 
 #HW Part3
+
     #What is a Python module and how does it differ from a Python package?
         #Modules contain ready-to-use codes. There are built-in modules in Python Standard Library (e.g. math) but one can make custom modules as well by creating e.g. functions. These modules can be imported. 
         #Packages are modules which contain sub-modules. These non built-in modules and sub-modules can be imported as well (e.g. os, random)
